8b93f985300e714a8f2fe7c00575995f32e3063c
Cansu Beyaz <62986760+cansubeyaz@users.noreply.github.com>
2024-06-02 12:59:14 +0200

    HOG Descriptor

---
+++ Exercise_6/mlrcv/hog_descriptor.py
@@ -0,0 +1,88 @@
+import numpy as np
+import cv2
+from mlrcv.core import *
+from typing import Optional
+
+def compute_magnitude(img: np.ndarray) -> np.ndarray:
+    """
+    This function computes the magnitudes for each pixel in an image:
+
+    Args:
+        - img (np.ndarray): image to compute the magnitude
+
+    Returns:
+        - magnitude (np.ndarray): magnitude computed for each pixel
+    """
+
+    magnitude = np.zeros((img.shape[0], img.shape[1]))
+
+    return magnitude
+
+def compute_angle(img: np.ndarray) -> np.ndarray:
+    """
+    This function computes the angles for each pixel in an image:
+
+    Args:
+        - img (np.ndarray): image to compute the magnitude
+
+    Returns:
+        - angle (np.ndarray): angles computed for each pixel
+    """
+
+    angle = np.zeros((img.shape[0], img.shape[1]))
+
+    return angle
+
+def create_cell_histograms(mag_cells: np.ndarray, ang_cells: np.ndarray, bin_size: Optional[int] = 9) -> np.ndarray:
+    """
+    This function computes the histograms for each cell in the image already divided into 8x8 cells:
+
+    Args:
+        - mag_cells (np.ndarray): magnitude values divided into cells
+        - ang_cells (np.ndarray): angles values divided into cells
+        - bin_size (int): number of bins on the histogram
+
+    Returns:
+        - bins (np.ndarray): Histogram calculated in each 8x8 cell in the image
+    """
+
+    bins = np.zeros((mag_cells.shape[0], mag_cells.shape[1], bin_size))
+    step_size = 180 / bin_size
+
+    return bins
+
+def dataloader(data_files: np.ndarray) -> np.ndarray:
+    """
+    This function load the images listed in data_files and compute the magnitudes and angles
+    to calculate the histograms to extract the hog features
+
+    Args:
+        - data_files (np.ndarray): list of image files to be loaded and generate the hog features
+
+    Returns:
+        - hog_feats (np.ndarray): hog features for all the images listed in data_files
+    """
+
+    data_feats = []
+    for fname in data_files:
+        img = load_image(fname)
+
+        ###########################################
+        # Implement here you function:
+        # - hist_cells should be output histogram from create_cell_histograms function
+        ###########################################
+
+        pass
+
+        ###########################################
+
+        hog_img = np.zeros((img.shape[0], img.shape[1]))
+        hog_img = build_hog_image(hog_img, hist_cells)
+        save_image(hog_img, fname)
+        feats = bins_to_feats(hist_cells)
+
+        data_feats.append(feats)
+
+    hog_feats = np.vstack(data_feats)
+
+    return hog_feats

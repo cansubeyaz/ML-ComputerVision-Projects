64aedaac5214e77197b840d82752376ccd724da0
Cansu Beyaz <62986760+cansubeyaz@users.noreply.github.com>
2024-06-05 12:08:17 +0200

    Transfer Learning

---
+++ Exercise_10/mlrcv/data_map.py
@@ -0,0 +1,20 @@
+class_to_lbl = {
+    #define here the data classes ex.:
+    # 0: "pen",
+    # 1: "microphone",
+    # ...
+}
+
+train_annotation = {
+    #define here the train data annotations:
+    # "xxx.jpg": 4,
+    # "xxy.jpg": 3,
+    # ...
+}
+
+val_annotation = {
+    #define here the val data annotations:
+    # "zzz.jpg": 0,
+    # "zzw.jpg": 7,
+    # ...
+}
